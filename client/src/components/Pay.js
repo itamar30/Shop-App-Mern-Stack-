d45ff4d250d14e316b40f58135bc7b376750f313
itamar berti <itamar.berti8@gmail.com>
2023-03-22 17:56:25 +0200

    Upload Files

--- client/src/components/Pay.js
+++ client/src/components/Pay.js
@@ -37,7 +37,7 @@ const Pay = () => {
     const makePayment = async () => {
       try {
         const res = await axios.post(
-          "http://localhost:5000/api/stripe/payment",
+          "https://shop-app-server-y65y.onrender.com/api/stripe/payment",
           {
             tokenId: stripeToken.id,
             amount: total,
