21ac129bf7637b7cdc1840390144d2ae8a2a158a
itamar berti <itamar.berti8@gmail.com>
2023-03-22 17:33:57 +0200

    Upload Files

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = "https://shop-app-server-y65y.onrender.com/api/";
 const TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZTNhY2YyMDZmYTBjNzhlODVkMjEzNyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3Njg4MTM0NX0.MRS2AbMTGefQyMyK6020kx73dKui25D1mo62ddf-JPg";
 
