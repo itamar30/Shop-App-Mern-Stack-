118c6a76003f1092961968dc1090002a07ab1ee9
itamar berti <itamar.berti8@gmail.com>
2023-03-22 17:42:34 +0200

    Upload Files

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -26,8 +26,8 @@ const Products = ({ cat, filters, sort }) => {
       try {
         const res = await axios.get(
           cat
-            ? `http://localhost:5000/api/products?category=${cat}`
-            : "http://localhost:5000/api/products"
+            ? `https://shop-app-server-y65y.onrender.com/api/products?category=${cat}`
+            : "https://shop-app-server-y65y.onrender.com/api/products"
         );
 
         setProducts(res.data);
